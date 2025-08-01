ecbed8f331ac4355ddf3bfde0aeca5a8e2479d59
Sandor Teleki <sandorteleki1@gmail.com>
2025-08-01 08:57:32 +0200

    feat: first text file read

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+        print(file_contents)
+
+def main ():
+    file_path =  "./books/frankenstein.txt"
+    get_book_text(file_path)
+
+main()
