4d675f70aa373d98a7b726317f86a2959a12881f
Sandor Teleki <sandorteleki1@gmail.com>
2025-08-02 06:59:30 +0200

    feat: book word length check

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
-        print(file_contents)
+        return file_contents
+
+def words_in_book(text):
+    words = text.split()
+    num_words = len(words)
+    num_words 
+    print(f"{num_words} words found in the document")
 
 def main ():
     file_path =  "./books/frankenstein.txt"
-    get_book_text(file_path)
+    # get_book_text(file_path)
+    words_in_book(get_book_text(file_path))
 
 main()
