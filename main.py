c9a34245db3a935c8fe66b9b937bd041b7537e3d
Sandor Teleki <sandorteleki1@gmail.com>
2025-08-07 09:23:01 +0200

    feat: added final status print

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
-from stats import words_in_book, number_of_characters
+from stats import words_in_book, number_of_characters, sorted_list_of_characters
+
 
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
         return file_contents
 
+def report(file_path, total_words, total_sorted_characters):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {total_words} total words")
+    print("--------- Character Count -------")
+    for item in total_sorted_characters:
+        key = list(item.keys())[0]
+        value = item[key]
+        if key.isalpha():
+            print(f"{key}: {value}")
+    print("============= END ===============")
+
 def main ():
     file_path =  "./books/frankenstein.txt"
-    words_in_book(get_book_text(file_path))
-    number_of_characters(get_book_text(file_path))
+    book_content = get_book_text(file_path)
+    total_words = words_in_book(book_content)
+    total_characters = number_of_characters(book_content)
+    total_sorted_characters = sorted_list_of_characters(total_characters)
+
+    report(file_path, total_words, total_sorted_characters)
+
 main()
--- stats.py
+++ stats.py
@@ -1,8 +1,7 @@
 def words_in_book(text):
     words = text.split()
     num_words = len(words)
-    num_words 
-    print(f"{num_words} words found in the document")
+    return num_words 
 
 def number_of_characters(text):
     letters = {}
@@ -12,5 +11,22 @@ def number_of_characters(text):
             letters[lowerCase] += 1
         else:
             letters[lowerCase] = 1
-    print(letters)
     return letters
+
+def sorted_list_of_characters(letters):
+    sorted_list = []
+    for letter in letters:
+        num = letters[letter]
+        d = {"char": letter, "num": num}
+        sorted_list.append(d)
+    sorted_list.sort(reverse=True, key=sort_on)
+    new_list = []
+    for d in sorted_list:
+        new_dict = {}
+        new_dict[d["char"]] = d["num"]
+        new_list.append(new_dict)
+    return new_list
+
+def sort_on(character):
+    return character["num"]
+
